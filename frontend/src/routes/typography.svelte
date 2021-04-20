3cf284802170ff0efba0fd20176b5b5ace20d4b0
Pontus Östlund <ponost@roxen.com>
2021-04-20 19:30:59 +0200

    Some typography mostly

--- frontend/src/routes/typography.svelte
+++ frontend/src/routes/typography.svelte
@@ -3,10 +3,48 @@
 </script>
 
 <svelte:head>
-  <title>{env('SITE_NAME', 'Disher')}</title>
+  <title>Typography - {env('SITE_NAME', 'Disher')}</title>
 </svelte:head>
 
 <div class="container py">
+  <div class="flow">
+    <h1>Lorem ipsum dolor sit amet</h1>
+
+    <p>
+      Consectetur adipiscing elit, <em>sed do eiusmod tempor</em> incididunt ut
+      labore et <a href="#quote-1">dolore magna</a> aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+
+    <h2>Ut enim ad minim veniam</h2>
+    <p>
+      Eget aliquet nibh praesent tristique. Facilisis leo vel fringilla est
+      ullamcorper eget nulla facilisi etiam. Magna sit amet purus gravida quis.
+      Urna id volutpat lacus laoreet non. Iaculis nunc sed augue lacus viverra
+      vitae congue eu. Venenatis urna cursus eget nunc. Tempus quam pellentesque
+      nec nam.
+    </p>
+
+    <blockquote id="quote-1">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <footer>Some Dude</footer>
+    </blockquote>
+
+    <p>
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+      deserunt mollit anim id est laborum. Varius quam quisque id diam vel quam.
+      Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Arcu non
+      odio euismod lacinia. Bibendum est ultricies integer quis auctor elit sed
+      vulputate mi. Nullam non nisi est sit amet. Morbi leo urna molestie at
+      elementum eu. Sodales ut etiam sit amet nisl purus in mollis.
+    </p>
+  </div>
+
+  <hr />
+
   <h1>Hello h1 world!</h1>
   <h2>Hello h2 world!</h2>
   <h3>Hello h3 world!</h3>
@@ -38,48 +76,12 @@
     congue gravida velit. Praesent sagittis nunc eget posuere consequat.
   </p>
 
+  <hr />
+
   <h1>Hello h1 world!<br />with linebreak</h1>
   <h2>Hello h2 world!<br />with linebreak</h2>
   <h3>Hello h3 world!<br />with linebreak</h3>
   <h4>Hello h4 world!<br />with linebreak</h4>
-
-  <hr />
-
-  <div class="flow">
-    <h1>Lorem ipsum dolor sit amet</h1>
-
-    <p>
-      Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </p>
-
-    <h2>Ut enim ad minim veniam</h2>
-    <p>
-      Eget aliquet nibh praesent tristique. Facilisis leo vel fringilla est
-      ullamcorper eget nulla facilisi etiam. Magna sit amet purus gravida quis.
-      Urna id volutpat lacus laoreet non. Iaculis nunc sed augue lacus viverra
-      vitae congue eu. Venenatis urna cursus eget nunc. Tempus quam pellentesque
-      nec nam.
-    </p>
-
-    <blockquote>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-      <footer>Some Dude</footer>
-    </blockquote>
-
-    <p>
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-      deserunt mollit anim id est laborum. Varius quam quisque id diam vel quam.
-      Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Arcu non
-      odio euismod lacinia. Bibendum est ultricies integer quis auctor elit sed
-      vulputate mi. Nullam non nisi est sit amet. Morbi leo urna molestie at
-      elementum eu. Sodales ut etiam sit amet nisl purus in mollis.
-    </p>
-  </div>
 </div>
 
 <style>
