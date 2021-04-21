ec60376a30e898eebf042c76b406b31277d9df09
Pontus Östlund <ponost@roxen.com>
2021-04-21 18:38:32 +0200

    Some input stuff

---
+++ frontend/src/components/form/Input.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import type { Maybe } from 'src/types'
+  import { createEventDispatcher } from 'svelte'
+
+  // Props
+  export let id: string
+  export let label: string
+  export let name: string
+  export let value: string | number = ''
+  export let placeholder: Maybe<string> = undefined
+  export let ariaLabel: string = ''
+  export let required = false
+  export let type:
+    | 'text'
+    | 'password'
+    | 'email'
+    | 'url'
+    | 'date'
+    | 'textarea'
+    | 'number' = 'text'
+
+  // Internals
+  let focused = value.toString().length > 0
+  const dispatch = createEventDispatcher()
+
+  function setFocus() {
+    focused = true
+  }
+
+  function unFocus() {
+    focused = value.toString().length > 0
+  }
+
+  // We can't bind the value on the input with a dynamic type.
+  // Hence this function
+  function handleInput(e: Event) {
+    const t = e.target as HTMLInputElement
+    value = type.match(/^(number|range)$/) ? Number(t.value) : t.value
+    dispatch('input', value)
+  }
+</script>
+
+<div class="form-group form-group--fancy" class:focused>
+  <label for={id}>{label}</label>
+  {#if type === 'textarea'}
+    <!-- prettier-ignore -->
+    <textarea
+      {name}
+      {id}
+      {placeholder}
+      aria-label={ariaLabel || label}
+      {required}
+      bind:value
+      on:focus={setFocus}
+      on:blur={unFocus}
+      ></textarea>
+  {:else}
+    <input
+      {type}
+      {name}
+      {id}
+      {placeholder}
+      {required}
+      aria-label={ariaLabel || label}
+      on:input={handleInput}
+      on:focus={setFocus}
+      on:blur={unFocus}
+    />
+  {/if}
+</div>
---
+++ frontend/src/routes/join/index.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { Maybe } from 'src/types'
+
+  import Input from '../../components/form/Input.svelte'
+
+  let email: Maybe<string>
+  let password: Maybe<string>
+
+  function handleEmail(e: CustomEvent) {
+    console.log(`email:`, e.detail)
+  }
+
+  function handlePassword(e: CustomEvent) {
+    console.log(`password:`, e.detail)
+  }
+</script>
+
+<div class="container my">
+  <h1>Join</h1>
+
+  <form class="my">
+    <Input
+      type="email"
+      name="email"
+      label="Email"
+      id="email"
+      required={true}
+      on:input={handleEmail}
+    />
+    <Input
+      type="password"
+      name="password"
+      label="Password"
+      id="password"
+      required={true}
+      on:input={handlePassword}
+    />
+
+    <div class="my">
+      <button class="button">Register</button>
+    </div>
+  </form>
+</div>
