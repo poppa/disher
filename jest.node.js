fa8e79c8fa6e83e05b1c5379d029ec7722b3e1e8
Pontus Östlund <ponost@roxen.com>
2021-04-24 08:35:02 +0200

    Cleaned up from previous project

--- jest.node.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const common = require(`${__dirname}/jest.common.js`)
-
-module.exports = {
-  ...common,
-  testEnvironment: 'node',
-}
